9a5143bff6985ea3e6828c47a817445df9bba7af
Leonardo da Costa Santos <leonardocsantoss@gmail.com>
2012-07-26 09:35:23 -0300

    fab deploy

--- settings.py
+++ settings.py
@@ -176,12 +176,3 @@ LOGIN_URL = "/"
 
 #Fechar a secao quando o browser fechar
 SESSION_EXPIRE_AT_BROWSER_CLOSE = True
-
-
-#Configurations for send email
-EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_HOST_USER = 'iellos@iellos.com'
-EMAIL_HOST_PASSWORD = '$iellos$++'
-EMAIL_SUBJECT_PREFIX = 'iellos '
-EMAIL_PORT = 587
-EMAIL_USE_TLS = True
