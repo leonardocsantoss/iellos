fa8a98be1ef3b676885d40ccaeaae4b126e3c2f4
Leonardo da Costa Santos <leonardocsantoss@gmail.com>
2012-07-26 09:54:49 -0300

    erro banco de imagens

--- apps/banco_de_imagens/views.py
+++ apps/banco_de_imagens/views.py
@@ -2,7 +2,7 @@
 from django.shortcuts import render_to_response, get_object_or_404
 from django.contrib.auth.decorators import login_required
 from django.template import RequestContext
-from django.http import HttpResponseRedirect, HttpResponse
+from django.http import HttpResponseRedirect, HttpResponse, Http404
 from django.core.paginator import Paginator, InvalidPage
 from django.views.decorators.csrf import csrf_exempt
 from django.core.urlresolvers import reverse
